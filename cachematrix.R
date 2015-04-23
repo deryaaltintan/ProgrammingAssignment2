23cd61bfeda56c8bb2480ece0286e5e6a7cf0046
deryaaltintan <derya.altntan@gmail.com>
2015-04-23 23:58:15 +0300

    example is added

--- cachematrix.R
+++ cachematrix.R
@@ -43,3 +43,25 @@ cacheSolve <- function(x, ...) {
   x$setinverse(t)
   t
 }
+
+#Example
+#> a <- makeCacheMatrix(matrix(c(1,2,3,4),nrow=2,ncol=2))
+#> a$get()
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> a$getinverse()
+#NULL
+#> cacheSolve(a)
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> a$getinverse()
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cacheSolve(a)
+#getting cached data
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
